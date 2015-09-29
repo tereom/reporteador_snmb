124ab42a8660b8c4a79123c5345ace1bdec45c4d
tereom <teresa.ortiz.mancera@gmail.com>
2015-09-29 12:29:43 -0500

    mejorar explicación en interfaz

--- shiny/server.R
+++ shiny/server.R
@@ -6,62 +6,72 @@ library(rmarkdown)
 library(DT)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  # terminación de la base de datos a considerar
-  pattern_db <- "\\.sqlite$"
-  
-  # función que crea los reportes cuando se presiona "generar_reportes"
-  crear_reportes <- eventReactive(input$actualiza, {
-    # leemos las rutas ingresadas en la applicación
-    ruta_entrega <- input$path_reporte
-    ruta_base <- input$path_sqlite
-    # si no se ingresó una ruta lo guarda en el escritorio
-    if(dirname(ruta_entrega) == "."){
-      ruta_entrega <- paste("../reportes/", ruta_entrega, sep = "")
-    }
+shinyServer(function(input, output, session) {
+    # terminación de la base de datos a considerar
+    pattern_db <- "\\.sqlite$"
     
-    dir.create(ruta_entrega)
-    # creamos nombres de reporte, carpetas y dbs como: fecha + nombre_entrega
-    fecha_reporte <- format(Sys.time(), "%Y_%m_%d")
-    entrega <- paste(fecha_reporte, "_", basename(ruta_entrega), sep = "")
-  
-    base_sqlite <- list.files(path = ruta_base, 
-      recursive = TRUE, full.names = TRUE, pattern = pattern_db)
+    # lista con retornos
+    resp <- list()
     
-    if(length(base_sqlite) > 0){
-      output_reporte = paste(entrega, ".docx", sep = "")
-      render('../scripts/revision_gral_iso.Rmd', output_file = output_reporte, 
-        output_dir = ruta_entrega)
-      
-      # tabla a desplegar en a app
-      resp <- list(tab_cgl)
-      
-      # revisamos su hay repetidos, y si hace falta creamos reporte y txt
-      conglomerado_reps <- collect(tbl(base_input, "Conglomerado_muestra"))
-      cgl_reps <- conglomerado_reps %>%
-        select(nombre, fecha_visita, id) 
-      ids_reps <- cgl_reps$id[duplicated(select(cgl_reps, nombre, fecha_visita))]
-      
-      if(length(ids_reps) > 0){
-        output_rep = paste(entrega, "_rep.docx", sep = "")
-        render('../scripts/revision_repetidos_iso.Rmd', output_file = output_rep, 
-          output_dir = ruta_entrega)
-      }
-	  imprimir <- paste("Se crearon los reportes en el directorio:", ruta_entrega)
-    }else{
-      imprimir <- "No se encontró niguna base de datos con terminación sqlite, 
-        considera que las rutas a archivos deben usar diagonales derechas (/)."
-    }
-
-  resp[[2]] <- imprimir
-	resp
+    # función que crea los reportes cuando se presiona "generar_reportes"
+    crear_reportes <- eventReactive(input$actualiza, {
+        # leemos las rutas ingresadas en la applicación
+        ruta_entrega <- input$path_reporte
+        ruta_base <- input$path_sqlite
+        # si no se ingresó una ruta lo guarda en el escritorio
+        if(dirname(ruta_entrega) == "."){
+            ruta_entrega <- paste("../reportes/", ruta_entrega, sep = "")
+        }
+        
+        dir.create(ruta_entrega)
+        # creamos nombres de reporte, carpetas y dbs como: fecha + nombre_entrega
+        fecha_reporte <- format(Sys.time(), "%Y_%m_%d")
+        entrega <- paste(fecha_reporte, "_", basename(ruta_entrega), sep = "")
+        
+        base_sqlite <- list.files(path = ruta_base,
+        recursive = TRUE, full.names = TRUE, pattern = pattern_db)
+        
+        if(length(base_sqlite) > 0){
+            output_reporte = paste(entrega, ".docx", sep = "")
+            render('../scripts/revision_gral_iso.Rmd', output_file = output_reporte,
+            output_dir = ruta_entrega)
+            
+            # tabla a desplegar en a app
+            resp[["tab_cgl"]] <- tab_cgl %>% arrange(cgl) %>% convert_to_encoding()
+            
+            # revisamos su hay repetidos, y si hace falta creamos reporte y txt
+            conglomerado_reps <- collect(tbl(base_input, "Conglomerado_muestra"))
+            cgl_reps <- conglomerado_reps %>%
+            select(nombre, fecha_visita, id)
+            ids_reps <- cgl_reps$id[duplicated(select(cgl_reps, nombre, fecha_visita))]
+            
+            if(length(ids_reps) > 0){
+                output_rep = paste(entrega, "_rep.docx", sep = "")
+                render('../scripts/revision_repetidos_iso.Rmd', output_file = output_rep,
+                output_dir = ruta_entrega)
+            }
+            imprimir <- paste("Los reportes se crearon en el directorio:", ruta_entrega)
+        }else{
+            imprimir <- "No se encontró niguna base de datos con terminación sqlite,
+            considera que las rutas a archivos deben usar diagonales derechas (/)."
+        }
+        
+        resp[["imprimir"]] <- imprimir
+        resp
+        
+    })
+    output$prueba <- renderText({
+        crear_reportes()[["imprimir"]]
+    })
+    output$tbl <- DT::renderDataTable(
+    crear_reportes()[["tab_cgl"]], 
+    options = list(lengthMenu = c(10, 15, 20, 30, -1), 
+    pageLength = 15,
+    searchHighlight = TRUE)
+    )
     
-  })
-  output$prueba <- renderText({
-    crear_reportes()[["imprimir"]]
-  })
-  output$tbl <- DT::renderDataTable(
-    crear_reportes()[["tab_cgl"]]
-  )
-  
+    session$onSessionEnded(function() {
+        stopApp()
+    })
 })
+
--- shiny/ui.R
+++ shiny/ui.R
@@ -3,24 +3,39 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-  # Application title
-  titlePanel("Revisión de registros SNMB"),
+# Application title
+titlePanel("Revisión de registros SNMB"),
+p("Esta aplicación genera reportes de entrega para una base de datos creada
+con el cliente de captura del SNMB, el resultado son dos archivos word, el
+primero contiene tablas que indican las secciones del cliente en que se
+capturó información y da medidas del volumen de información almacenada,
+número de archivos, número de grabaciones, número de fotos, etc.
 
-  # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      textInput("path_sqlite", "Ruta a base de datos sqlite:", value = ""),
-      textInput("path_reporte", "Nombre del reporte:", value = ""),
-      helpText("Si solo se provee el nombre del reporte se guardará en la 
-        carpeta reportes (dentro
-        del directorio de la aplicación), si deseas guardarlo en otra ubicación 
-        se debe especificar la ruta junto con el nombre, 
-        ej: C:/Users/juan/Documents/reporte1"),
-      actionButton("actualiza", "generar reportes")
-  ), 
-  mainPanel(
-      textOutput("prueba"),
-      DT::dataTableOutput("tbl")
-    )
-  )
+El segundo archivo se genera únicamente cuando
+se detectan conglomerados repetidos en la base de datos, este reporte
+indica si la información en los repetidos es la misma. Adicionalmente, se
+crea una copia de la base de datos sqlite, esto en caso de que se desee
+explorar esta base "),
+br(),
+# Sidebar with a slider input for the number of bins
+sidebarLayout(
+sidebarPanel(
+textInput("path_sqlite", "Ruta a base de datos sqlite:", value = ""),
+helpText("Especificar la ruta a la carpeta donde esta almacenada
+la base de datos, basta con especificar la carpeta del cliente,
+ej: C:/Users/juan/Desktop/cliente_snmb"),
+textInput("path_reporte", "Nombre del reporte:", value = ""),
+helpText("Especificar la ruta a la carpeta donde se almacenarán los
+reportes y el nombre que identifique los reportes,
+ej: em(C:/Users/juan/Doesktop/reporte_1), si solo se provee el
+nombre del reporte, este se guardará en la carpeta reportes (dentro del
+directorio de la aplicación)."),
+actionButton("actualiza", "generar reportes")
+),
+mainPanel(
+textOutput("prueba"),
+br(),
+DT::dataTableOutput("tbl")
+)
+)
 ))
